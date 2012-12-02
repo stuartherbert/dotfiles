3994153a8362449721ba34004f88cb4184a0d453
Stuart Herbert <stuart@stuartherbert.com>
2012-12-02 14:50:16 +0000

    Display current folder in XTERM title

--- bash/90-prompt.sh
+++ bash/90-prompt.sh
@@ -48,6 +48,15 @@ function _prompt_command() {
     else
         PS1='${shell_chroot:+($shell_chroot)}'"`_git_prompt`"'\u@\h:\w\$ '
     fi
+
+	# If this is an xterm set the title to user@host:dir
+	case "$TERM" in
+		xterm*|rxvt*)
+    		PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
+    		;;
+		*)
+    		;;
+	esac
 }
 
 if [[ -z $PROMPT_COMMAND ]]; then
