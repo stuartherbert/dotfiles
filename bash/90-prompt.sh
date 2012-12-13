8a15e200d1de90ce055b6d2b6cf918d472d4ed82
Stuart Herbert <stuart.herbert@datasift.com>
2012-12-13 10:09:47 +0000

    New: shortened bash prompt

--- bash/90-prompt.sh
+++ bash/90-prompt.sh
@@ -25,6 +25,79 @@ function _git_prompt() {
     fi
 }
 
+# Simple BASH function that shortens
+# a very long path for display by removing
+# the left most parts and replacing them
+# with a leading ...
+#
+# the first argument is the path
+#
+# the second argument is the maximum allowed
+# length including the '/'s and ...
+#
+# from: http://hbfs.wordpress.com/2009/09/01/short-pwd-in-bash-prompts/
+function _shorten_path ()
+{
+	x=${1}
+	
+	# replace $HOME with ~ character to save space
+    x="${x/#${HOME}/~}"
+
+	len=${#x}
+	max_len=$2
+
+	if [[ $len -gt $max_len ]]
+	then
+		# finds all the '/' in
+		# the path and stores their
+		# positions
+		#
+		pos=()
+		for ((i=0;i<len;i++)) ; do
+			if [ "${x:i:1}" == "/" ] ; then
+				pos=(${pos[@]} $i)
+			fi
+		done
+		pos=(${pos[@]} $len)
+  
+		# we have the '/'s, let's find the
+		# left-most that doesn't break the
+		# length limit
+		#
+		i=0
+		while [ $((len-pos[i])) -gt $((max_len-3)) ] ; do
+			i=$((i+1))
+		done
+
+		# let us check if it's OK to
+		# print the whole thing
+		#
+		if [ ${pos[i]} == 0 ] ; then
+			# the path is shorter than
+			# the maximum allowed length,
+			# so no need for ...
+			#
+			echo -n "${x}"
+      
+		elif [ ${pos[i]} == $len ] ; then
+			# constraints are broken because
+			# the maximum allowed size is smaller
+			# than the last part of the path, plus
+			# '...'
+			#
+			echo -n "...${x:((len-max_len+3))}"
+		else
+			# constraints are satisfied, at least
+			# some parts of the path, plus ..., are
+			# shorter than the maximum allowed size
+			#
+			echo -n "...${x:pos[i]}"
+		fi
+	else
+		echo -n "${x}"
+	fi
+}
+
 # uncomment for a colored prompt, if the terminal has the capability; turned
 # off by default to not distract the user: the focus in a terminal window
 # should be on the output of commands, not on the prompt
@@ -44,7 +117,7 @@ fi
 
 function _prompt_command() {
     if [ "$color_prompt" = yes ]; then
-        PS1='${shell_chroot:+($shell_chroot)}'"`_git_prompt`"'\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+        PS1='${shell_chroot:+($shell_chroot)}'"`_git_prompt`"'\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]`_shorten_path ${PWD} 15`\[\033[00m\]\$ '
     else
         PS1='${shell_chroot:+($shell_chroot)}'"`_git_prompt`"'\u@\h:\w\$ '
     fi
