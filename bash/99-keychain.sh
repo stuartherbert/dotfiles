5ef4bf36476cdbcafd2ef78ea87857d8300a37b2
Stuart Herbert <stuart.herbert@datasift.com>
2012-12-28 14:08:56 +0000

    Tweak: only run the keychain if it is installed

--- bash/99-keychain.sh
+++ bash/99-keychain.sh
@@ -1,3 +1,7 @@
-keychain ~/.ssh/id_dsa
-. ~/.keychain/$HOSTNAME-sh
-. ~/.keychain/$HOSTNAME-sh-gpg
+# do we *have* keychain in the first place?
+if `which keychain` ; then
+	keychain ~/.ssh/id_dsa
+	for x in ~/.keychain/$HOSTNAME-* ; do
+		. $x
+	done
+fi
