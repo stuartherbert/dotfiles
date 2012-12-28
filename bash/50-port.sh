6c0fb91bcd8dc3d2c0c422ff68983741473476a3
Stuart Herbert <stuart@stuartherbert.com>
2012-12-28 14:54:41 +0000

    New: support Python modules on OSX installed via ports

--- bash/50-port.sh
+++ bash/50-port.sh
@@ -4,3 +4,8 @@ for x in /opt/local/bin /opt/local/sbin ; do
 		export PATH="$x:$PATH"
 	fi
 done
+
+# add the Macports Python folder to the path that Python looks in
+if [[ -d /opt/local/Library/Frameworks/Python.framework ]] ; then
+	export PYTHONPATH="/opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages:$PYTHONPATH"
+fi
