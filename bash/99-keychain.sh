077128d9a41d8f25e1dcf25ba249334341d12783
Stuart Herbert <stuart@stuartherbert.com>
2013-01-03 11:44:33 +0000

    Fix: keychain now looks at multiple keys, does not source non-bash scripts

--- bash/99-keychain.sh
+++ bash/99-keychain.sh
@@ -1,7 +1,14 @@
 # do we *have* keychain in the first place?
 if `which keychain` ; then
-	keychain ~/.ssh/id_dsa
-	for x in ~/.keychain/$HOSTNAME-* ; do
+	keys=
+	for x in id_dsa id_rsa ; do
+		key=~/.ssh/$x
+		if [[ -f $key ]] ; then
+			keys="${keys} $key"
+		fi
+	done
+	keychain $keys
+	for x in ~/.keychain/$HOSTNAME-sh ; do
 		. $x
 	done
 fi
