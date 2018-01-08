9bcf7ba51980d4d7830ec34ab10bb557d5113839
Stuart Herbert <stuart@stuartherbert.com>
2018-01-08 07:44:41 +0000

    Fix: git prompt update

--- bash/90-prompt.sh
+++ bash/90-prompt.sh
@@ -18,7 +18,7 @@ function _git_prompt() {
             branch=${BASH_REMATCH[1]}
         else
             # Detached HEAD.  (branch=HEAD is a faster alternative.)
-            branch="(`git describe --all --contains --abbrev=4 HEAD 2> /dev/null ||
+            branch="(`git describe --tags --exact-match || git describe --all --contains --abbrev=4 HEAD 2> /dev/null ||
                 echo HEAD`)"
         fi
         echo -n '\[\e[0;37;'"$ansi"';1m\]'"$branch"'\[\e[0m\] '
