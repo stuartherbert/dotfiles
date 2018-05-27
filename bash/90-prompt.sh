ec32173df8a5386c99d297011604c9aa62260cbc
Stuart Herbert <stuart@stuartherbert.com>
2018-05-27 20:14:23 +0100

    Fix: update git prompt for latest git version

--- bash/90-prompt.sh
+++ bash/90-prompt.sh
@@ -5,8 +5,8 @@ esac
 
 # function for git info to add to prompt
 function _git_prompt() {
-    local git_status="`git status -unormal 2>&1`"
-    if ! [[ "$git_status" =~ Not\ a\ git\ repo ]]; then
+    local git_status=$(git status -unormal 2>&1)
+    if ! [[ "$git_status" =~ fatal ]]; then
         if [[ "$git_status" =~ nothing\ to\ commit ]]; then
             local ansi=42
         elif [[ "$git_status" =~ nothing\ added\ to\ commit\ but\ untracked\ files\ present ]]; then
@@ -18,7 +18,7 @@ function _git_prompt() {
             branch=${BASH_REMATCH[1]}
         else
             # Detached HEAD.  (branch=HEAD is a faster alternative.)
-            branch="(`git describe --all --contains --abbrev=4 HEAD 2> /dev/null ||
+            branch="(`git describe --tags --exact-match || git describe --all --contains --abbrev=4 HEAD 2> /dev/null ||
                 echo HEAD`)"
         fi
         echo -n '\[\e[0;37;'"$ansi"';1m\]'"$branch"'\[\e[0m\] '
